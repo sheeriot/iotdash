67aee5edbea83e21a0eb64ee2994580779ec990b
Kris Thompson <148518+krisdthompson@users.noreply.github.com>
2022-11-15 06:01:58 -0600

    Update for new DashStack (#2)
    * better startup
    
    * setup nginx
    
    * fp
    
    * readme cleaning
    
    Co-authored-by: Kris Thompson <kthompson@netgate.com>

--- azure/vm0/netsec.tf
+++ azure/vm0/netsec.tf
@@ -5,61 +5,112 @@ resource "azurerm_network_security_group" "surveyor" {
 }
 
 resource "azurerm_network_security_rule" "ssh_src1" {
-  name                        = "ssh_from_${var.ssh_src1name}"
   priority                    = 101
+  name                        = "ssh_from_${var.ssh_src1name}"
+  source_address_prefix       = var.ssh_src1
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "22"
-  source_address_prefix       = var.ssh_src1
   destination_address_prefix  = azurerm_network_interface.surveyor.private_ip_address
   resource_group_name         = azurerm_resource_group.vm.name
   network_security_group_name = azurerm_network_security_group.surveyor.name
 }
 
 resource "azurerm_network_security_rule" "ssh_src2" {
-  name                        = "ssh_from_${var.ssh_src2name}"
   priority                    = 102
+  name                        = "ssh_from_${var.ssh_src2name}"
+  source_address_prefix       = var.ssh_src2
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "22"
-  source_address_prefix       = var.ssh_src2
   destination_address_prefix  = azurerm_network_interface.surveyor.private_ip_address
   resource_group_name         = azurerm_resource_group.vm.name
   network_security_group_name = azurerm_network_security_group.surveyor.name
 }
 
-resource "azurerm_network_security_rule" "tcp8000_src1" {
-  name                        = "tcp8000_from_${var.ssh_src1name}"
+resource "azurerm_network_security_rule" "http80" {
   priority                    = 111
+  name                        = "tcp80-http_from_any"
+  source_address_prefix       = "*"
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "8000"
-  source_address_prefix       = var.ssh_src1
+  destination_port_range      = "80"
   destination_address_prefix  = azurerm_network_interface.surveyor.private_ip_address
   resource_group_name         = azurerm_resource_group.vm.name
   network_security_group_name = azurerm_network_security_group.surveyor.name
 }
 
-resource "azurerm_network_security_rule" "tcp8000_src2" {
-  name                        = "tcp8000_from_${var.ssh_src2name}"
+resource "azurerm_network_security_rule" "http8000" {
   priority                    = 112
+  name                        = "tcp8000-http_from_any"
+  source_address_prefix       = "*"
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "8000"
-  source_address_prefix       = var.ssh_src2
   destination_address_prefix  = azurerm_network_interface.surveyor.private_ip_address
   resource_group_name         = azurerm_resource_group.vm.name
   network_security_group_name = azurerm_network_security_group.surveyor.name
 }
-
+resource "azurerm_network_security_rule" "https443" {
+  priority                    = 113
+  name                        = "tcp8883-https_from_any"
+  source_address_prefix       = "*"
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  destination_address_prefix  = azurerm_network_interface.surveyor.private_ip_address
+  resource_group_name         = azurerm_resource_group.vm.name
+  network_security_group_name = azurerm_network_security_group.surveyor.name
+}
+resource "azurerm_network_security_rule" "tcp8883_src1" {
+  priority                    = 114
+  name                        = "tcp8883-mqtts_from_${var.ssh_src1name}"
+  source_address_prefix       = var.ssh_src1
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "8883"
+  destination_address_prefix  = azurerm_network_interface.surveyor.private_ip_address
+  resource_group_name         = azurerm_resource_group.vm.name
+  network_security_group_name = azurerm_network_security_group.surveyor.name
+}
+resource "azurerm_network_security_rule" "tcp8083_src1" {
+  priority                    = 115
+  name                        = "tcp8083-mqtt-wss_from_${var.ssh_src1name}"
+  source_address_prefix       = var.ssh_src1
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "8083"
+  destination_address_prefix  = azurerm_network_interface.surveyor.private_ip_address
+  resource_group_name         = azurerm_resource_group.vm.name
+  network_security_group_name = azurerm_network_security_group.surveyor.name
+}
+resource "azurerm_network_security_rule" "tcp1883_src1" {
+  priority                    = 116
+  name                        = "tcp1883-mqtt_from_${var.ssh_src1name}"
+  source_address_prefix       = var.ssh_src1
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "1883"
+  destination_address_prefix  = azurerm_network_interface.surveyor.private_ip_address
+  resource_group_name         = azurerm_resource_group.vm.name
+  network_security_group_name = azurerm_network_security_group.surveyor.name
+}
 resource "azurerm_network_interface_security_group_association" "iotdash" {
   network_interface_id      = azurerm_network_interface.surveyor.id
   network_security_group_id = azurerm_network_security_group.surveyor.id
--- readme.md
+++ readme.md
@@ -1,24 +1,27 @@
-SheerIoT is an abrubt focus on making IoT operational.
-
 # IoT Dash
-### Why am I here?
+
+SheerIoT is an abrupt focus on making IoT technologies operational through knowledge sharing and collaboration.
+
+## Why am I here?
 
 This repository is used to house infrastructure-as-code (IaC) for deploying IoT Dashboard Applications.
 
 For documentation and collaboration purposes, strong preference is for an Open-Source approach and corresponding application choices.
 
 Typical software applications:
+
 * Mosquitto - MQTT Broker
 * Nginx for Web Hosting, Reverse-Proxy, Load-balancing)
 * InfluxDB for Time Series Database
-* Django for Custom Apps
 * Grafana for Ad Hoc Query and Dashboards
+* Django for Custom Apps
 
 This projects is focussed on Infrastructure as Code (IaC) using Terraform.
 
 ## Building Blocks
 
 ### Azure Infrastructure
+
 The Azure subfolder contains terraform for building an Azure Infrastructure (network and VM) for hosting an IoT application.
 
 ### RF Field Surveyor
@@ -29,8 +32,4 @@ The RF Field Surveyor is used to validate LoRaWAN Network Coverage. It contains 
 
 * Packet Tracker - inspect RF Performance by Device
 * ClimateWAN - graphs temperature, humidity, battery by device
-* Surveyor - the GPS Pages provide maps of device location, signal strength, gateway, distance. Very powerful for field validation. 
-
-
-
-#### 
+* Surveyor - the GPS Pages provide maps of device location, signal strength, gateway, distance. Very powerful for field validation.

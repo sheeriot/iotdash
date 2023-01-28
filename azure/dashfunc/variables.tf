c5ac0cb7e8e6da8b9c32f69b1c68fc906987644d
Kris Thompson <kris@austinvoicedata.com>
2023-01-28 19:26:33 +0000

    update variables

--- azure/dashfunc/variables.tf
+++ azure/dashfunc/variables.tf
@@ -24,13 +24,11 @@ variable "env_name" {
   type        = string
   description = "Name the Environment"
 }
-# variable "cidr_block" { 
-#   type        = string
-#   description = "CIDR Block Definition e.g. 10.250.128.0/17"
-# }
 variable "timezone" {
   default = "America/Chicago"
 }
-variable "timezone" {
-  kv_owner = "78070f1c-ddb0-47dd-b1a4-d4d3f3c2b954"
+variable "kv_owner" {
+  type = string
+  description = "admin user for portal access"
+  default = "78070f1c-ddb0-47dd-b1a4-d4d3f3c2b954"
 }
